54d0363acdf59c8d2a2cb4a301bd972fb3aab36e
ShahriyarHridoy <35798678+ShahriyarHridoy@users.noreply.github.com>
2019-07-08 23:40:13 +0600

    learning project

--- SelfieHelper/app/src/main/java/com/example/selfiehelper/ui/activity/MainMenuActivity.java
+++ SelfieHelper/app/src/main/java/com/example/selfiehelper/ui/activity/MainMenuActivity.java
@@ -8,14 +8,42 @@ import android.view.View;
 
 import com.example.selfiehelper.R;
 import com.example.selfiehelper.ui.activity.hotspot.HotspotGroupChat;
+import com.nightonke.boommenu.Animation.BoomEnum;
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
+import com.nightonke.boommenu.BoomButtons.TextOutsideCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+import com.nightonke.boommenu.Piece.PiecePlaceEnum;
+
+import java.lang.reflect.Array;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    BoomMenuButton bmb;
+    Integer[] imgSrcArray={R.drawable.selfiehelper_logo_icon, R.drawable.selfiehelper_logo_icon,R.drawable.selfiehelper_logo_icon, R.drawable.selfiehelper_logo_icon, R.drawable.selfiehelper_logo_icon};
+    String[] textTitle= {"1", "2", "3", "4", "5"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        bmb = findViewById(R.id.bmb);
+
+        bmb.setButtonEnum(ButtonEnum.TextOutsideCircle);
+        bmb.setButtonPlaceEnum(ButtonPlaceEnum.SC_5_3);
+        bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_5_3);
+
+
+        for (int i=0; i< bmb.getPiecePlaceEnum().pieceNumber(); i++){
+
+            TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder().normalImageRes(imgSrcArray[i]).normalText(textTitle[i]);
+            bmb.addBuilder(builder);
+        }
     }
+
+
     public void intentStreamVideo(View view) {
         Intent i =  new Intent(MainMenuActivity.this, StartStreamActivity.class);
         startActivity(i);
